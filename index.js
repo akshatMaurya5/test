c3f43c3a85e9e210f1492df88c5864ccba6368c8
akshatMaurya5 <108023349+akshatMaurya5@users.noreply.github.com>
2023-12-29 22:59:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hemlo");
+[
+	{ "keys": ["ctrl+d"], "command": "toggle_comment", "args": { "block": false } },
+	{"keys": ["ctrl+shift+a"], "command": "reindent", "args": {"single_line": false}},
+	{ "keys": ["ctrl+shift+down"], "command": "duplicate_line" },
+	{ "keys": ["ctrl+alt+b"], "command": "cancel_build" }
+]
