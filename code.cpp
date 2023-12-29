950ad7912096ab31e93892cb402cb1653150e888
akshatMaurya5 <mauryaakshat5@gmail.com>
2023-12-29 22:38:33 +0530

    .

---
+++ code.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // cout<<"akshat maurya || SWE @ Google";
+    int n;
+    cin>>n;
+
+    cout<<n;
+
+
+    return 0;
+}
---
+++ test.py
@@ -0,0 +1,3 @@
+n=input("Enter the value:")
+
+print(n)
